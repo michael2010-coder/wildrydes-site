111b605c5fd14385beecd69049f2ee3e28ae5f93
michael2010-coder <michaelnwachukwu2010@gmail.com>
2025-08-08 16:59:17 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_5IpfLIeHR', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '16u0gs4smohh7to2pds9uaugmp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_ArBX6ohaU', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6g8am76chgirullqolef2a9baa', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
